55edaad8bf122259e028eba5bc4e659e4abde2e0
Alejandro Echavarria <aechavarriaj@gmail.com>
2024-10-07 11:29:45 -0500

    function navbar main style ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+'use client'
 import NavBar from "../components/ui/NavBar/NavBar";
+import { useTranslations } from "next-intl";
+import styled from "styled-components";
+const DivHero = styled.div`
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
 
 export default function Home() {
+  const traduction = useTranslations("Home")
   return (
     <div>
       <NavBar />
-      <h1>Hello</h1>
+      <DivHero>      
+        <h1>{traduction("title")}</h1>
+        <h3>{traduction("description")}</h3>
+      </DivHero>
     </div>
   )
 }
--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -1,18 +1,68 @@
 'use client'
 import Link from "next/link";
 import styled from "styled-components";
+import { useTranslations } from "next-intl";
+import SelectLanguage from "../SelectLanguage/SelectLanguage";
 
 const Nav = styled.nav`
     display: flex;
+    width: 100%;
     justify-content: space-around;
+    height: 100px;
+    background-color: gray;
+    align-items: center;
+
+    a {
+        text-decoration: none;
+        color: #0c0c0c;
+        font-size: 20px;
+    }
+
+    a:hover{
+        color: white;
+    }
+
+    `
+
+const DivLogo = styled.div`
+    display: flex;
+    width: 50%;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    h1{
+        font-size: 44px;
+        font-weight: bold;
+        text-align: center;
+    }
+
+    span{
+        font-size: 34px;
+        color: white;
+        margin-left: 5px;
+    }
     
     `
 
-export default function NavBar(){
+const DivLinks = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 50%;
+
+
+    `
+
+export default function NavBar():React.ReactElement{
+    const traduction = useTranslations("NavBar")
     return(
-        <Nav>
-                <Link href="#">Inicio</Link>
-                <Link href="/auth/signin">Entrar</Link>
+        <Nav>   
+            <DivLogo><h1>Riwi<span>Shop</span></h1></DivLogo>
+            <DivLinks>
+                <Link href="/">{traduction("home")}</Link>
+                <Link href="/auth/signin">{traduction("login")}</Link>
+            </DivLinks>
+            <div><SelectLanguage /></div>
         </Nav>
     )
 }
